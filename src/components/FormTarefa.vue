db71c116429b4993e716f6cd61c769811adee3f2
Simon <77901095+jfsimon-pro@users.noreply.github.com>
2024-06-05 14:23:59 -0300

    Update FormTarefa.vue

--- src/components/FormTarefa.vue
+++ src/components/FormTarefa.vue
@@ -6,15 +6,17 @@
         <div class="column">
             <div class="is-flex is-align-items-center is-justify-content-space-between">
                 <section>
-                    <strong>00:00</strong>
+                    <strong>
+                        {{ tempoDecorrido }}
+                    </strong>
                 </section>
-                <button class="button">
+                <button class="button" @click="iniciar">
                     <span class="icon">
                         <i class="fas fa-play"></i>
                     </span>
                     <span>play</span>
                 </button>
-                <button class="button">
+                <button class="button" @click="finalizar">
                     <span class="icon">
                         <i class="fas fa-stop"></i>
                     </span>
@@ -29,6 +31,27 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 export default defineComponent({
-    name: 'FormTarefa'
+    name: 'FormTarefa',
+    data(){
+        return {
+            tempoEmSegundo: 0,
+            cronometro: 0
+        }
+    },
+    computed: {
+        tempoDecorrido () : string {
+            return new Date(this.tempoEmSegundo * 1000).toISOString().substring(11,19)
+        }
+    },
+    methods: {
+        iniciar() {
+            this.cronometro = setInterval(()=>{
+                this.tempoEmSegundo += 1;
+            }, 1000)            
+        },
+        finalizar() {
+            clearInterval(this.cronometro)
+        }
+    }
 })
 </script>
