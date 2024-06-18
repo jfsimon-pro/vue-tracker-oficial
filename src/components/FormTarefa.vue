f385678f0ced2755b9bf1b3b8fd5d02be588a58b
Simon <77901095+jfsimon-pro@users.noreply.github.com>
2024-06-18 16:30:14 -0300

    a

--- src/components/FormTarefa.vue
+++ src/components/FormTarefa.vue
@@ -4,54 +4,18 @@
             <input type="text" class="input" placeholder="Qual tarefa você deseja inciiar?">
         </div>
         <div class="column">
-            <div class="is-flex is-align-items-center is-justify-content-space-between">
-                <section>
-                    <strong>
-                        {{ tempoDecorrido }}
-                    </strong>
-                </section>
-                <button class="button" @click="iniciar">
-                    <span class="icon">
-                        <i class="fas fa-play"></i>
-                    </span>
-                    <span>play</span>
-                </button>
-                <button class="button" @click="finalizar">
-                    <span class="icon">
-                        <i class="fas fa-stop"></i>
-                    </span>
-                    <span>stop</span>
-                </button>
-            </div>
-
+            <TemporizadorNovo />
         </div>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import TemporizadorNovo from "./TemporizadorNovo.vue"
 export default defineComponent({
     name: 'FormTarefa',
-    data(){
-        return {
-            tempoEmSegundo: 0,
-            cronometro: 0
-        }
-    },
-    computed: {
-        tempoDecorrido () : string {
-            return new Date(this.tempoEmSegundo * 1000).toISOString().substring(11,19)
-        }
-    },
-    methods: {
-        iniciar() {
-            this.cronometro = setInterval(()=>{
-                this.tempoEmSegundo += 1;
-            }, 1000)            
-        },
-        finalizar() {
-            clearInterval(this.cronometro)
-        }
+    components:{
+        TemporizadorNovo
     }
 })
 </script>
